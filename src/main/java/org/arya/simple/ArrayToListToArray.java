eb094df2de54c3b847835017db6b1cfc3e218c2c
Muniswmay <59533001+Muniswamy366@users.noreply.github.com>
2025-06-19 11:20:11 +0000

    Add Java programs to find missing number in array (sum and XOR methods)

--- src/main/java/org/arya/simple/ArrayToListToArray.java
+++ src/main/java/org/arya/simple/ArrayToListToArray.java
@@ -2,7 +2,6 @@ package org.arya.simple;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ArrayToListToArray {
 
@@ -12,15 +11,8 @@ public class ArrayToListToArray {
 		List<String> list1 = Arrays.asList(names);
 		System.out.println(list1);
 
-		List<String> list2 = arrayToListJava8(names);
-		System.out.println(list2);
-
 		String[] strings1 = listToArrayJava8(list1);
 		System.out.println(Arrays.toString(strings1));
-	}
-
-	public static List<String> arrayToListJava8(String[] strings) {
-		return Arrays.stream(strings).collect(Collectors.toList());
 	}
 
 	public static String[] listToArrayJava8(List<String> list) {
--- src/main/java/org/arya/simple/NumberPalindrome.java
+++ src/main/java/org/arya/simple/NumberPalindrome.java
@@ -1,22 +1,38 @@
 package org.arya.simple;
 
+/**
+ * A simple program to check if a given integer is a palindrome.
+ * Negative numbers are not considered palindromes.
+ */
 public class NumberPalindrome {
 
     public static void main(String[] args) {
 
-        int num = 121;
+        int inputNumber = 121;
 
-        int result = reverse(num);
+        long reversedNum = reverse(inputNumber);
 
-        if (result == num)
+        if (reversedNum == -1) {
+            System.out.println("Negative numbers are not considered palindromes");
+        } else if (reversedNum == inputNumber) {
             System.out.println("palindrome");
-        else
+        } else {
             System.out.println("not palindrome");
-
+        }
     }
 
-    private static int reverse(int number) {
-        int reverse = 0;
+    /**
+     * Reverses the digits of the given integer.
+     * If the input number is negative, returns -1.
+     *
+     * @param number the integer to reverse
+     * @return the reversed number, or -1 if input is negative
+     */
+    private static long reverse(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        long reverse = 0;
 
         while (number != 0) {
             reverse = reverse * 10 + number % 10;
